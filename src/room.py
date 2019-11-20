b5a353f4729acf945e10e0593ba34b1eef1a2bdf
atolmie <atolmie22@gmail.com>
2019-11-19 22:01:55 -0600

    created room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,27 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+#class = noun
+#attributes = adjectives 
+#self = this 
+#self refers to the instance of the class
+#repr provides more info for devs 
+
+class Room:
+    """This Room has information"""  
+    def __init__(self, name, description=[],):
+        self.name = name 
+        self.description = description
+
+    def __str__(self):
+        return 'I am in a state of despair'
+
+    def __repr__(self):
+        return f'Room({self.name}, {self.description})'
+
+    
+
+
+adventure_room = Room('Room Of Despair', description=['ghosts', 'crying', 'darkness', 'defeat'])
+print(repr(adventure_room)) 
+print(str(adventure_room))
